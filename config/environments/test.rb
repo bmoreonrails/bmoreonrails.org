e25c6e8cf3bb3410cd4080fbddc412dbf9747206
Matt Scilipoti <matt@scilipoti.name>
2016-05-24 20:29:11 -0400

    tests: run in random order
    - removes deprecation warning.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,4 +33,7 @@ BmoreonrailsOrg::Application.configure do
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
+
+  # run tests in random order
+  config.active_support.test_order = :random
 end
