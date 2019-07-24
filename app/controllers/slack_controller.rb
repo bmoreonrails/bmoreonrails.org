b697eb5dcf85fc4405fcc29266d6952046f60c91
Doug Price <dprice@fearless.tech>
2019-07-24 10:05:17 -0400

    Check that the captcha was passed before sending the invite

--- app/controllers/slack_controller.rb
+++ app/controllers/slack_controller.rb
@@ -2,10 +2,14 @@ class SlackController < ApplicationController
   def invite
     invite = SlackInvite.new(email: params[:email])
     
-    if invite.deliver
-      flash[:info] = 'Awesome, check your email for an invite!'
+    if verify_recaptcha(model: invite)
+      if invite.deliver
+        flash[:info] = 'Awesome, check your email for an invite!'
+      else
+        flash[:error] = invite.errors.full_messages.to_sentence
+      end
     else
-      flash[:error] = invite.errors.full_messages.to_sentence
+      flash[:error] = 'Sorry, you need to tell us you are not a robot to join our slack!'
     end
 
     redirect_to root_path
